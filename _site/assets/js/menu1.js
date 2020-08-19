1e480c0f49ff41978a1b5ae65bb2fcd6f5ac175d
Mike Williams <manicmike66@gmail.com>
2020-08-19 17:02:17 +1000

    rearranged the fs

---
+++ _site/assets/js/menu1.js
@@ -0,0 +1,24 @@
+addMenu('Menu a','Home')
+ addMenuItem('Menu a','1a','home','../a.main/index.htm','home')
+ addMenuItem('Menu a','2a','shop','../a.main/shop.htm','shop')
+ addMenuItem('Menu a','3a','about me','../a.main/about.htm','about')
+ addMenuItem('Menu a','4a','media','../media/index.htm','media')
+ addMenuItem('Menu a','5a','contact','../a.main/contact.htm','contact')
+ addMenuItem('Menu a','6a','links','../a.main/links.htm','links')
+ 
+addMenu('Menu b','Machines')
+ addMenuItem('Menu b','1b','start here','../machines/index.htm','Start Here')
+ addMenuItem('Menu b','2b','FAQ','../machines/faq.htm','FAQ')
+ addMenuItem('Menu b','3b','price list','../pricelist/index.htm','Price List')
+ addMenuItem('Menu b','4b','stock','../stock/index.htm','Stock Items')
+ addMenuItem('Menu b','5b','service tips','../serv/index.htm','Service Tips')
+ addMenuItem('Menu b','6b','gallery','../machines/gal.htm','Gallery')
+
+addMenu('Menu c','Sewing')
+ addMenuItem('Menu c','1c','overview','../sewing/index.htm','Overview')
+ addMenuItem('Menu c','2c','casual wear','../sewing/cas.htm','Casual')
+ addMenuItem('Menu c','3c','bone couture','../bone/index.htm','Couture')
+ addMenuItem('Menu c','4c','costume','../sewing/cos.htm','Costume')
+ addMenuItem('Menu c','5c','size guide','../sewing/wo.size.htm','Size Guide')
+
+  
